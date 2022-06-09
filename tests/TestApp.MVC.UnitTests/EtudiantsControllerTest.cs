be0129374ec5ee52a7a9edc5b7c20bda7c0eaef1
hinault <ngdonfack@gmail.com>
2022-06-09 12:44:00 -0400

    Update EtudiantsControllerTest.cs

--- tests/TestApp.MVC.UnitTests/EtudiantsControllerTest.cs
+++ tests/TestApp.MVC.UnitTests/EtudiantsControllerTest.cs
@@ -263,25 +263,6 @@ namespace TestApp.MVC.UnitTests
         }
         
         
-        [Fact]
-        public async Task Edit_HttpPost_EtudiantValide_Retourne_RedirectToActionIndex2()
-        {
-            //Etant donné
-            var fixture = new Fixture();
-            var etudiant = fixture.Create<Etudiant>();
-
-            var mockEtudiantsService = new Mock<IEtudiantsService>();
-            mockEtudiantsService.Setup(e => e.ModifierAsync(It.IsAny<Etudiant>()));
-
-            var etudiantsController = new EtudiantsController(mockEtudiantsService.Object);
-
-            //Quand
-            var redirectToActionResult = await etudiantsController.Edit(1, etudiant) as RedirectToActionResult;
-
-            //Alors
-            redirectToActionResult.Should().NotBeNull();
-            redirectToActionResult.ActionName.Should().Be("Post");
-            mockEtudiantsService.Verify(e => e.ModifierAsync(It.IsAny<Etudiant>()));
-        }
+       
     }
 }
